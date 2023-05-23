87c58313fb4571a86727fdf4e00ab8591dbd358c
anjun65 <hajrulkhaira@gmail.com>
2023-05-23 14:41:37 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@pcteam.polibatam" />
+        <meta name="description" content="PCTeam adalah COE asal Polibatam yang bergerak dibidang keamanan jaringan, pengembangan aplikasi yang aman. Website Aman, penetration testing, sistem informasi dan lain-lain">
+        <meta name="keywords" content="Penetration Testing, Keamanan Jaringan, Mobile Apps Development, Web Development, PCTeam, Politeknik Negeri Batam">
+        <meta name="author" content="Polibatam Cyber Team" />
+        <meta image="{{ asset('img/pcteam.jpg') }}">
+        <meta property="og:type" content="article">
+        <meta property="og:image" content="{{ asset('img/pcteam.jpg') }}">
+        <meta property="og:title" content="Penetration Testing, Keamanan Jaringan, Mobile Apps Development, Web Development, PCTeam, Politeknik Negeri Batam">
+        <meta property="og:site_name" content="Polibatam Cyber Team">
+        <meta property="og:url" content="{{ route('home') }}">
+        <meta property="og:description" content="PCTeam adalah COE asal Polibatam yang bergerak dibidang keamanan jaringan, pengembangan aplikasi yang aman. Website Aman, penetration testing, sistem informasi dan lain-lain">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}" />
+        <link rel="apple-touch-icon" href="{{ asset('logo/apple-touch-icon.png') }}">
+        <link rel="canonical" href="{{ route('home') }}">
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased">
+        <x-banner />
+
+        <div class="min-h-screen bg-gray-100">
+            @livewire('navigation-menu')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<x-guest-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:max-w-4xl">
+                <img class="mx-auto h-56" src="{{ asset('img/pcteam.png') }}" alt="">
+                <figure class="mt-10">
+                <blockquote class="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+                    <p>Coming Soon</p>
+                    <p>Stay Tuned</p>
+                </blockquote>
+            </figure>
+        </div>
+        </div>
+    </div>
+</x-guest-layout>
