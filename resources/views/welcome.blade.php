b3874c2eb0f31e04fd4e341ab2e9a67a2c4e64d9
anjun65 <hajrulkhaira@gmail.com>
2023-05-26 11:10:53 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mx-auto max-w-2xl lg:max-w-4xl">
-                <img class="mx-auto h-56" src="{{ asset('img/pcteam.png') }}" alt="">
+                <img class="mx-auto w-auto h-56" src="{{ asset('img/pcteam.png') }}" alt="">
                 <figure class="mt-10">
                 <blockquote class="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
                     <p>Coming Soon</p>
